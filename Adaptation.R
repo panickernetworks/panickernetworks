e34805ef0bd052f1250fc6ba2cbddfb72a12b764
Panicker <91792013+panickernetworks@users.noreply.github.com>
2023-08-30 23:24:37 +0530

    Add files via upload

---
+++ Adaptation.R
@@ -0,0 +1,31 @@
+#adapt and evolve
+#L*L
+#3 particle
+#trait 1: reflect from boundaries
+#initial condition 3 distant co-ordinate
+#random walk- run and tumble and rectilliear 
+#trait
+#vision field
+#Field a scattered will kill
+#offspring also shud have this feature
+#B proliferating seeds, probability p of replication increase
+#adapt to A area
+#vision fieldil varumbo aanu trajectory decide cheyyan pattunnath
+#first tim esee another being nin recognisation
+#higher chance that they will follow towards each
+#sharing each other information
+#initially memory exchange based on time sharing
+#each have double the memory
+#after exchange, leave
+#within the circle, B comes
+#memory exchange, uncommon aayittulla locations explore
+#status of each field A, b, particle- information
+#chances of survival by interacting is 50
+#whn interact with A, survival 50, danger 50
+#mobilty k oru score
+#can it share information with you. yes or no
+#static aayit oru aale 
+#static kandal depending on the past memory, it will avoid
+#reliable information
+#cooperation or deception
+ 
